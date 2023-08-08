befa6af5894839f9dca1bceb5f95304c65c69f25
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-08 09:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let container=document.createElement('div');
+container.className="container";
+let line=document.createElement('div');
+line.id="line";
+
+container.append(line);
+
+document.body.append(container);
